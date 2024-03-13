0d409cd0992f6322f32172be06ebdd4c5696019f
Dominic Nidy <dominicnidy3@gmail.com>
2024-03-13 15:00:26 -0400

    Moved readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="randgraph",
-    version="0.1.1",
+    version="0.1.2",
     description="A package for generating and drawing randomly generated graphs",
     author="Dominic Nidy",
     author_email="domnidy01@gmail.com",
