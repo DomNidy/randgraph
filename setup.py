d8ae72b3ef7f86ee49e5fc0ebebf1811614d3b91
Dominic Nidy <dominicnidy3@gmail.com>
2024-03-13 14:58:18 -0400

    Moved repository root

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="randgraph",
+    version="0.1.1",
+    description="A package for generating and drawing randomly generated graphs",
+    author="Dominic Nidy",
+    author_email="domnidy01@gmail.com",
+    url="https://github.com/DomNidy/randgraph",
+    packages=find_packages(),
+    install_requires=["matplotlib", "networkx"],
+)
